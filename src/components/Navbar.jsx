ade186c060d7174d91ec0b91bfbf82b54a7fc52f
EmsTravel <emacondulet@yahoo.com>
2023-05-16 14:54:06 +0200

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css' 
+import Logo from '../../assets/logo.png'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='Navbar_logo'>
+        <img src='logo.png' alt='kasa, location appartements'/>
+      </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
