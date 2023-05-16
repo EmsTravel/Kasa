0590c339cfecce327c8cc88406b76cde6bc20144
EmsTravel <emacondulet@yahoo.com>
2023-05-16 15:54:06 +0200

    fonts added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Navbar.css' 
-import Logo from '../../assets/logo.png'
+
 
 function Navbar() {
   return (
     <nav className='navbar'>
       <div className='Navbar_logo'>
-        <img src='logo.png' alt='kasa, location appartements'/>
+        <img src='logo.png' alt='kasa, location git '/>
       </div>
+      <div>Accueil</div>
+      <div>Apropo</div>
+
 
     </nav>
   )
