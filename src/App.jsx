0cb24e22f6ba12f7f177d51d285782adfbb6aaf7
EmsTravel <emacondulet@yahoo.com>
2023-05-25 12:44:47 +0200

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Banner from './components/Banner'
 import ApartementGrid from './components/ApartementGrid'
 import Main from './components/Main'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Banner/>
         <ApartementGrid/>
         </Main>
+        <Footer/>
      
     </div>
   )
